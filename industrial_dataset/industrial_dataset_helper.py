5cf7d9a6b1780b656c53b418d0f0e05a6d683f81
Jon Azpiazu <jonazpiazu@users.noreply.github.com>
2023-11-16 16:36:29 +0100

    Revert to single zip file hosted in github releases

--- industrial_dataset/industrial_dataset_helper.py
+++ industrial_dataset/industrial_dataset_helper.py
@@ -11,6 +11,7 @@ import py7zr
 import requests
 from natsort import natsorted
 import json
+import zipfile
 
 
 @dataclass(frozen=False)
@@ -21,6 +22,10 @@ class IndustrialDataset:
     def __post_init__(self):
         with open("industrial_dataset_list.json") as json_file:
             self.file_index = json.load(json_file)
+        if not self.file_index.get(self.scene):
+            logging.error(f'Dataset {self.scene} does not exist.')
+            return None
+
         if not self.data_root:
             logging.debug("Using default download path")
             self.data_root = os.environ.get("HOME") + "/open3d_data"
@@ -34,35 +39,28 @@ class IndustrialDataset:
         if not extract_base_dir.exists():
             extract_base_dir.mkdir()
 
-        for file_url in self.file_index.get(self.scene):
-            destination_file = download_path / Path(file_url.split("/")[-1])
-            if destination_file.exists():
-                logging.debug("File already downloaded, skipping download")
-            else:
-                logging.info(f"Downloading {file_url}")
-                self.download_file(file_url, destination_file)
-                logging.info(f"Downloaded to {destination_file}")
+        destination_file = download_path / Path(self.file_index.get(self.scene).split("/")[-1])
+        if destination_file.exists():
+            logging.debug("File already downloaded, skipping download")
+        else:
+            logging.info(f"Downloading {self.file_index.get(self.scene)}")
+            self.download_file(self.file_index.get(self.scene), destination_file)
+            logging.info(f"Downloaded to {destination_file}")
 
         extract_path = extract_base_dir / Path(self.scene)
         self.paths = None
         if not extract_path.exists():
             extract_path.mkdir()
-            logging.info(f"Created directory {extract_path}")
+            logging.info(f'Created directory {extract_path}')
+        
+            logging.info(f'Extracting {destination_file}')
+            self.paths = self.unzip_file(destination_file, extract_path)
+            logging.info(f'Extracted to {extract_path}')
+            self.paths = [str(extract_path / Path(x)) for x in self.paths]
 
-            compressed_target = download_path / destination_file.stem
-            logging.info(f"Extracting {compressed_target}")
-            with multivolumefile.open(compressed_target, mode="rb") as target_archive:
-                with py7zr.SevenZipFile(target_archive, "r") as archive:
-                    archive.extractall(extract_path)
-                    self.paths = [
-                        str(Path(extract_path) / Path(f.filename))
-                        for f in archive.list()
-                    ]
-            logging.info(f"Extracted to {extract_path}")
-
-        else:
+        if not self.paths: 
             self.paths = [str(f) for f in extract_path.iterdir() if f.is_file()]
-
+        
         self.paths = natsorted(self.paths)
 
     def download_file(self, url, destination_file):
@@ -73,6 +71,11 @@ class IndustrialDataset:
             return destination_file
         else:
             return None
+
+    def unzip_file(self, zip_file_path, extract_to_folder):
+        with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+            zip_ref.extractall(extract_to_folder)
+        return zip_ref.namelist()
 
 
 if __name__ == "__main__":
@@ -85,7 +88,7 @@ if __name__ == "__main__":
         pcd = o3d.io.read_point_cloud(pcd_path)
         print(pcd)
 
-    dataset = IndustrialDataset(scene="EvenTableSinglePartRealsense")
+    dataset = IndustrialDataset(scene="UnEvenTableSinglePartZivid")
     for pcd_path in dataset.paths:
         pcd = o3d.io.read_point_cloud(pcd_path)
         print(pcd)
